27a7f5332c983946fb853387cc518c281db93aa7
Charles Koko <55494612+charleskoko@users.noreply.github.com>
2024-10-24 23:21:46 +0200

    Remove french comments

--- tests/BlaspCheckValidationTest.php
+++ tests/BlaspCheckValidationTest.php
@@ -12,16 +12,12 @@ class BlaspCheckValidationTest extends TestCase
      */
     public function test_blasp_check_validation_passes_with_clean_text()
     {
-        // Texte sans profanité
         $data = ['message' => 'This is a clean message.'];
 
-        // Validation avec la règle 'blasp_check'
         $rules = ['message' => 'blasp_check'];
 
-        // Exécuter la validation
         $validator = Validator::make($data, $rules);
 
-        // Vérifier que la validation passe
         $this->assertTrue($validator->passes());
     }
 
@@ -32,16 +28,12 @@ class BlaspCheckValidationTest extends TestCase
      */
     public function test_blasp_check_validation_fails_with_profanity()
     {
-        // Texte avec profanité
         $data = ['message' => 'This is a fucking message.'];
 
-        // Validation avec la règle 'blasp_check'
         $rules = ['message' => 'blasp_check'];
 
-        // Exécuter la validation
         $validator = Validator::make($data, $rules);
 
-        // Vérifier que la validation échoue
         $this->assertTrue($validator->fails());
     }
 
@@ -52,16 +44,12 @@ class BlaspCheckValidationTest extends TestCase
      */
     public function test_blasp_check_validation_passes_with_clean_french_text()
     {
-        // Texte sans profanité en français
         $data = ['message' => 'Ceci est un message propre.'];
 
-        // Validation avec la règle 'blasp_check'
         $rules = ['message' => 'blasp_check:fr'];
 
-        // Exécuter la validation
         $validator = Validator::make($data, $rules);
 
-        // Vérifier que la validation passe
         $this->assertTrue($validator->passes());
     }
 
@@ -72,16 +60,12 @@ class BlaspCheckValidationTest extends TestCase
      */
     public function test_blasp_check_validation_fails_with_french_profanity()
     {
-        // Texte avec profanité en français
         $data = ['message' => 'Ceci est un message de merdique.'];
 
-        // Validation avec la règle 'blasp_check'
         $rules = ['message' => 'blasp_check:fr'];
 
-        // Exécuter la validation
         $validator = Validator::make($data, $rules);
 
-        // Vérifier que la validation échoue
         $this->assertTrue($validator->fails());
     }
 }
