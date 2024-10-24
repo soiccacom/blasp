755a48ca25971ab2dbfd6e7cd2779f99e1e0b929
Charles Koko <55494612+charleskoko@users.noreply.github.com>
2024-10-24 22:03:32 +0200

    Update BlaspCheckFrenchTest.php

--- tests/BlaspCheckFrenchTest.php
+++ tests/BlaspCheckFrenchTest.php
@@ -10,15 +10,14 @@ use Illuminate\Support\Facades\Config;
 class BlaspCheckFrenchTest extends TestCase
 {
 
+    protected $blaspService;
+
     public function setUp(): void
     {
         parent::setUp();
-
-        Config::set('blasp.profanities', [
-            'con', 'connard', 'connasse', 'merde', 'putain', 'enculé', 'salope', 'batard', 'fils de pute', 'pute', 'cul', 'abruti',
-            'crétin', 'imbécile', 'ordure', 'débile', 'foutre', 'chiant', 'chiotte', 'bouffon','merdique','bordel'
-        ]);
+        Config::set('blasp.profanities', ['fr' => ['putain', 'connasse', 'c0nn4ss3', 'putain', 'connard', 'merdique', 'bordel', 'foutre', 'Putain', 'merde']]);
         Config::set('blasp.separators', [' ', '-', '_']);
+        Config::set('blasp.false_positives', ['fr' => ['passeur', 'classe']]);
         Config::set('blasp.substitutions', [
             '/a/' => ['a', '4', '@', 'Á', 'á', 'À', 'Â', 'à', 'Â', 'â', 'Ä', 'ä', 'Ã', 'ã', 'Å', 'å', 'æ', 'Æ', 'α', 'Δ', 'Λ', 'λ'],
             '/b/' => ['b', '8', '\\', '3', 'ß', 'Β', 'β'],
@@ -48,80 +47,100 @@ class BlaspCheckFrenchTest extends TestCase
             '/z/' => ['z', 'Ζ', 'ž', 'Ž', 'ź', 'Ź', 'ż', 'Ż'],
 
         ]);
+        $this->blaspService = new BlaspService('fr');
+    }
+
+    public function french_test_data_provider(): array
+    {
+        return [
+            'test_real_blasp_service' => [
+                'string' => 'Ceci est une putain de phrase',
+                'hasProfanity' => true,
+                'count' => 1,
+                'expectedText' => 'Ceci est une ****** de phrase',
+            ],
+            'test_straight_match' => [
+                'string' => 'Ceci est une connasse',
+                'hasProfanity' => true,
+                'count' => 1,
+                'expectedText' => 'Ceci est une ********',
+            ],
+            'test_substitution_match' => [
+                'string' => 'Ceci est une c0nn4ss3',
+                'hasProfanity' => true,
+                'count' => 1,
+                'expectedText' => 'Ceci est une ********',
+            ],
+            'test_obscured_match' => [
+                'string' => 'Ceci est une c-o-n-n-a-s-s-e',
+                'hasProfanity' => true,
+                'count' => 1,
+                'expectedText' => 'Ceci est une ***************',
+            ],
+            'test_doubled_match' => [
+                'string' => 'Ceci est une ccoonnaassee',
+                'hasProfanity' => true,
+                'count' => 1,
+                'expectedText' => 'Ceci est une ************',
+            ],
+            'test_combination_match' => [
+                'string' => 'Ceci est une c-ooonn4ss3',
+                'hasProfanity' => true,
+                'count' => 1,
+                'expectedText' => 'Ceci est une ***********',
+            ],
+            'test_multiple_profanities_no_spaces' => [
+                'string' => 'merdiqueputain foutre',
+                'hasProfanity' => true,
+                'count' => 3,
+                'expectedText' => '************** ******',
+            ],
+            'test_multiple_profanities' => [
+                'string' => 'Ceci est une putain de phrase de connard !',
+                'hasProfanity' => true,
+                'count' => 2,
+                'expectedText' => 'Ceci est une ****** de phrase de ******* !',
+            ], 'test_scunthorpe_problem' => [
+                'string' => 'je suis un passeur',
+                'hasProfanity' => false,
+                'count' => 0,
+                'expectedText' => 'je suis un passeur',
+            ], 'test_paragraph' => [
+                'string' => "Ce projet est tellement merdique. C'est un vrai bordel, et personne n'en a rien à foutre.",
+                'hasProfanity' => true,
+                'count' => 3,
+                'expectedText' => "Ce projet est tellement ********. C'est un vrai ******, et personne n'en a rien à ******.",
+            ],
+        ];
+    }
+
+
+    /**
+     * @throws Exception
+     * @dataProvider french_test_data_provider
+     */
+    public function test_language_french(string $string, bool $hasProfanity, int $profanitiesCount, string $expectedText): void
+    {
+        $result = $this->blaspService->check($string);
+
+        $this->assertEquals($hasProfanity, $result->hasProfanity);
+        $this->assertSame($profanitiesCount, $result->profanitiesCount);
+        $this->assertSame($expectedText, $result->cleanString);
     }
 
     /**
      * @throws Exception
      */
-    public function test_real_blasp_service_french()
+    public function test_paragraph_french(): void
     {
-        $blaspService = new BlaspService();
+        $paragraph = "Ce projet est tellement merdique. C'est un vrai bordel, et personne n'en a rien à foutre.";
 
-        $result = $blaspService->check('Ceci est une putain de phrase','fr');
+        $result = $this->blaspService->check($paragraph, 'fr');
 
+        $expectedOutcome = "Ce projet est tellement ********. C'est un vrai ******, et personne n'en a rien à ******.";
         $this->assertTrue($result->hasProfanity);
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function test_straight_match_french()
-    {
-        $blaspService = new BlaspService();
-
-        $result = $blaspService->check('Ceci est une connasse', 'fr');
-
-        $this->assertTrue($result->hasProfanity);
-        $this->assertSame(1, $result->profanitiesCount);
-        $this->assertCount(1, $result->uniqueProfanitiesFound);
-        $this->assertSame('Ceci est une ********', $result->cleanString);
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function test_substitution_match_french()
-    {
-        $blaspService = new BlaspService();
-
-        $result = $blaspService->check('Ceci est une c0nn4ss3','fr');
-
-        $this->assertTrue($result->hasProfanity);
-        $this->assertSame(1, $result->profanitiesCount);
-        $this->assertCount(1, $result->uniqueProfanitiesFound);
-        $this->assertSame('Ceci est une ********', $result->cleanString);
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function test_multiple_profanities_french()
-    {
-        $blaspService = new BlaspService();
-
-        $result = $blaspService->check('Ceci est une putain de phrase de connard !');
-
-        $this->assertTrue($result->hasProfanity);
-        $this->assertSame(2, $result->profanitiesCount);
-        $this->assertCount(2, $result->uniqueProfanitiesFound);
-        $this->assertSame('Ceci est une ****** de phrase de ******* !', $result->cleanString);
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function test_paragraph_french()
-    {
-        $blaspService = new BlaspService();
-
-        $paragraph = "Ce projet est tellement merdique. C'est un vrai bordel, et personne n'en a rien à foutre. Putain, je n'en peux plus de cette merde.";
-
-        $result = $blaspService->check($paragraph, 'fr');
-
-        $expectedOutcome = "Ce projet est tellement *******. C'est un vrai ******, et personne n'en a rien à ******. ******, je n'en peux plus de cette *****.";
-        $this->assertTrue($result->hasProfanity);
-        $this->assertSame(5, $result->profanitiesCount);
-        $this->assertCount(5, $result->uniqueProfanitiesFound);
+        $this->assertSame(3, $result->profanitiesCount);
+        $this->assertCount(3, $result->uniqueProfanitiesFound);
         $this->assertSame($expectedOutcome, $result->cleanString);
     }
 }
