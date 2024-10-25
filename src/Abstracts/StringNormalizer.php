37ac373cf8e75c1274b550df3ffd567f94540ddb
Charles Koko <55494612+charleskoko@users.noreply.github.com>
2024-10-25 16:55:34 +0200

    Removal of unnecessary elements in the normalizer class

--- src/Abstracts/StringNormalizer.php
+++ src/Abstracts/StringNormalizer.php
@@ -5,19 +5,6 @@ namespace Blaspsoft\Blasp\Abstracts;
 abstract class StringNormalizer
 {
 
-    /**
-     * Package active language
-     *
-     * @var string|null
-     */
-    private ?string $language;
-
-
-    public function __construct(?string $language = null)
-    {
-        $this->language = $language;
-    }
-
     abstract public function normalize(string $string): string;
 
 }
